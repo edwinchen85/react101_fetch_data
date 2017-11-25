1a3ad124508c9d3add321d2fc654bb45aec64e6f
edwinchen85 <edwinchen85@gmail.com>
2017-11-25 17:14:01 +0800

    Using the Fetch API

--- src/js/App.js
+++ src/js/App.js
@@ -3,46 +3,61 @@ import image from '../images/cloud-upload-download-data-transfer.svg';
 import Collapsible from './Collapsible';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          isLoading: true,
-          contacts: []
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      contacts: []
     }
+  }
 
-    componentDidMount() {
-      this.fetchData();
-    }
+  componentDidMount() {
+    this.fetchData();
+  }
 
-    fetchData() {
+  fetchData() {
+    fetch('https://randomuser.me/api/?results=50&nat=us,dk,fr,gb')
+    .then(response => response.json())
+    .then(parsedJSON => parsedJSON.results.map(user => (
+      {
+        name: `${user.name.first} ${user.name.last}`,
+        username: `${user.login.username}`,
+        email: `${user.email}`,
+        location: `${user.location.street}, ${user.location.city}`
+      }
+    )))
+    .then(contacts => this.setState({
+      contacts,
+      isLoading: false
+    }))
+    .catch(error => console.log('parsing failed', error));
+  }
 
-    }
-
-    render() {
-      const {isLoading, contacts} = this.state;
-        return (
-            <div>
-                <header>
-                    <img src={image} />
-                    <h1>Fetching Data <button className="btn btn-sm btn-danger">Fetch now</button></h1>
-                </header>
-                <div className={`content ${isLoading ? 'is-loading' : ''}`}>
-                    <div className="panel-group">
-                        {
-                            !isLoading && contacts.length > 0 ? contacts.map(contact => {
-                                return <Collapsible title="Tracy Palmer">
-                                    <p>tracey.palmer@example.com<br />3280 manchester road, ely</p>
-                                </Collapsible>
-                            }) : null
-                        }
-                    </div>
-                    <div className="loader">
-                        <div className="icon"></div>
-                    </div>
-                </div>
+  render() {
+    const {isLoading, contacts} = this.state;
+    return (
+      <div>
+        <header>
+          <img src={image} />
+          <h1>Fetching Data <button className="btn btn-sm btn-danger">Fetch now</button></h1>
+        </header>
+          <div className={`content ${isLoading ? 'is-loading' : ''}`}>
+            <div className="panel-group">
+              {
+                !isLoading && contacts.length > 0 ? contacts.map(contact => {
+                  const {username, name, email, location} = contact;
+                  return <Collapsible key={username} title={name}>
+                    <p>{email}<br />{location}</p>
+                  </Collapsible>
+                }) : null
+              }
             </div>
-        );
-    }
+              <div className="loader">
+                <div className="icon"></div>
+              </div>
+          </div>
+      </div>
+    );
+  }
 }
 export default App;
